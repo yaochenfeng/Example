f50f08ed49e4bbb0533d7e24ece1ded09c14e81e
yaochenfeng <yaochenfeng@1024ul.com>
2023-04-26 19:03:54 +0800

    feat: pod模板初始化

--- fastlane/actions/auto_pod_new.rb
+++ fastlane/actions/auto_pod_new.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 module Fastlane
   module Actions
     module SharedValues
@@ -8,29 +9,70 @@ module Fastlane
     class AutoPodNewAction < Action
       def self.run(params)
         # fastlane will take care of reading in the parameter and fetching the environment variable:
-        UI.message "pod name: #{params[:pod_name]}"
-        pod_repo = params[:pod_repo]
-        UI.message "pod 使用私有源仓库: #{pod_repo}"
-        
-        if pod_repo
-          Actions.lane_context[SharedValues::AUTO_POD_REPO] = pod_repo
-          
-        end
-        self.changePodfile()
-        # command = []
-        # result = Actions.sh(command.join(' '))
+        UI.message "pod name: #{params[:pod_name]} #{user_name}"
+        genPodspec(params)
+        genSource(params)
         UI.success("Successfully new pod ⬆️ ")
         # return result
       end
-      # podfile文件替换
-      def self.changePodfile()
-        podfile = 'Podfile'
-        #替换
-        if File.exist?(podfile) == false
-          UI.message "podfie 不存在"
-          return
-        end
-        content = File.readlines podfile
+      def self.genSource(params)
+        path = "Sources/#{params[:pod_name]}/"
+        FileUtils.mkdir_p(path) unless File.exists?(path)
+        content = "@_exported import RXKit"
+        File.open(path + "Export.swift", "w") { |file| file.puts content }
+      end
+      def self.genPodspec(params)
+        podName = params[:pod_name]
+        podspec = %Q`
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
+Pod::Spec.new do |s|
+  s.name = '#{podName}'
+  s.author = { "#{user_name}" => "#{user_email}" }
+  s.homepage = "https://github.com/#{user_name}/#{podName}"
+  s.source           = { :git => "https://github.com/#{user_name}/#{podName}.git", :tag => s.version.to_s }
+  
+`
+        podspec << %q`
+  # pod
+  s.version = "0.1.0"
+  s.summary = "#{s.name}"
+    s.description = <<-DESC
+    "#{s.name}" description
+  DESC
+    s.license = {
+      :type => "MIT",
+      :text => <<-LICENSE,
+  copyright MIT
+  LICENSE
+  }
+  s.platform = :ios, "11.0"
+  s.swift_version = "5"
+  s.source_files = "Sources/#{s.name}/**/*.swift"
+  s.dependency "RXKit"
+end
+  `
+      File.open("#{podName}.podspec", "w") { |file| file.puts podspec }
+      end
+      def self.user_name
+        (ENV['GIT_COMMITTER_NAME'] || github_user_name || `git config user.name` || `<GITHUB_USERNAME>` ).strip
+      end
+  
+      def self.github_user_name
+        github_user_name = `security find-internet-password -s github.com | grep acct | sed 's/"acct"<blob>="//g' | sed 's/"//g'`.strip
+        is_valid = github_user_name.empty? or github_user_name.include? '@'
+        return is_valid ? nil : github_user_name
+      end
+  
+      def self.user_email
+        (ENV['GIT_COMMITTER_EMAIL'] || `git config user.email`).strip
+      end
+  
+      def self.year
+        Time.now.year.to_s
+      end
+  
+      def self.date
+        Time.now.strftime "%m/%d/%Y"
       end
       #####################################################
       # @!group Documentation
@@ -41,9 +83,7 @@ module Fastlane
       end
 
       def self.details
-        # Optional:
-        # this is your chance to provide a more detailed description of this action
-        "pod 模板生成文件处理"
+        "pod 模板生成文件生成处理"
       end
 
       def self.available_options
@@ -51,11 +91,10 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :pod_name,
                                        env_name: "AUTO_POD_NAME", # The name of the environment variable
                                        description: "pod名称", # a short description of this parameter
-                                       is_string: true),
-          FastlaneCore::ConfigItem.new(key: :pod_repo,
-                                       env_name: "AUTO_POD_REPO", # The name of the environment variable
-                                       description: "pod私有repo仓库,不使用空", # a short description of this parameter
-                                       is_string: true)
+                                       verify_block: proc do |value|
+                                        UI.user_error!("pod 名称 `pod_name: 'name'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                     end)
         ]
       end
 
