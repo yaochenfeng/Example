4bcd4b5a2f0d3c26a79567a4c5f93bbdfc6fa50a
yaochenfeng <yaochenfeng@1024ul.com>
2023-04-26 15:24:29 +0800

    pod 二进制化

--- fastlane/actions/auto_pod_bin.rb
+++ fastlane/actions/auto_pod_bin.rb
@@ -7,17 +7,27 @@ module Fastlane
     class AutoPodBinAction < Action
       def self.run(params)
         # fastlane will take care of reading in the parameter and fetching the environment variable:
-        UI.message "Parameter API Token: #{params[:pod_name]}"
+        podName = params[:pod_name]
+        configuration = params[:configuration]
+        UI.message "PodName: #{podName} configuration: #{configuration}"
 
         # sh "shellcommand ./path"
 
         # Actions.lane_context[SharedValues::AUTO_POD_BIN_CUSTOM_VALUE] = "my_val"
-        command = []
-        command << 'xcodebuild'
-        command << 'clean build'
-        command << "-target #{params[:pod_name]}"
-        command << "-project Pods/Pods.xcodeproj"
+        command = ['xcodebuild -project Pods/Pods.xcodeproj']#-sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS' ONLY_ACTIVE_ARCH=NO ARCHS='i386 x86_64' BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" ENABLE_BITCODE=YES OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode clean build']
+        command << "-scheme #{podName}"
+        command << "-configuration #{configuration}"
+        command << "-sdk iphonesimulator"
+        command << "clean build"
         result = Actions.sh(command.join(' '))
+
+        command = ['xcodebuild -project Pods/Pods.xcodeproj']#-sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XS' ONLY_ACTIVE_ARCH=NO ARCHS='i386 x86_64' BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" ENABLE_BITCODE=YES OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode clean build']
+        command << "-scheme #{params[:pod_name]}"
+        command << "-configuration #{configuration}"
+        command << "-sdk iphoneos"
+        command << "build"
+        result = Actions.sh(command.join(' '))
+        other_action.create_xcframework(frameworks: ["build/#{configuration}-iphonesimulator/#{podName}/#{podName}.framework", "build/#{configuration}-iphoneos/#{podName}/#{podName}.framework"], output: "build/#{podName}.xcframework")
       end
 
       #####################################################
@@ -45,13 +55,14 @@ module Fastlane
                                        verify_block: proc do |value|
                                           UI.user_error!("pod 名称 `pod_name: 'name'`") unless (value and not value.empty?)
                                           # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
-                                       end)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :configuration,
+                                        description: "configuration", # a short description of this parameter
+                                        default_value: "Release")
         ]
       end
 
       def self.output
-        # Define the shared values you are going to provide
-        # Example
         [
           ['AUTO_POD_BIN_CUSTOM_VALUE', 'A description of what this value contains']
         ]
