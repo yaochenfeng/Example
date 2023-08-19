7d47dd2256f63e5f1629ebac09ec0013b57a3ccc
yaochenfeng <282696845@qq.com>
2023-08-19 23:27:43 +0800

    feat: add App Playground

---
+++ Sources/Example.swiftpm/Example/ExampleApp.swift
@@ -0,0 +1,14 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Example")
+        }
+    }
+}
